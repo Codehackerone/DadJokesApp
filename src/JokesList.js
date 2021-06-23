85ff08f8a6d1363b5db64079a4489e6f5a5137b7
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-23 18:23:09 +0530

    jokelist updated

--- src/JokesList.js
+++ src/JokesList.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import axios from "axios";
 
 class JokeList extends Component{
+    static defaultProps={
+        numJokesToGet:10
+    };
+    constructor(props){
+        super(props);
+        this.state={jokes:[]};
+    }
+    async componentDidMount(){
+        //Load Jokes
+        let jokes=[];
+        while(jokes.length<this.props.numJokesToGet){
+            let res=await axios.get("https://icanhazdadjoke.com",{headers:{Accept:"application/json"}});
+            jokes.push(res.data.joke);
+        }
+        this.setState({jokes:jokes});
+    }
     render(){
         return(
-            <div>
+            <div className="JokeList">
                 <h1>Dad Jokes</h1>
+                <div className="JokeList-joke"></div>
+                {this.state.jokes.map(j=>(
+                    <div>{j}</div>
+                ))}
             </div>
         )
     }
