37498ad0eba677140e4e4d7dc36056be6193088a
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-24 15:25:16 +0530

    local storage added

--- src/JokesList.js
+++ src/JokesList.js
@@ -10,11 +10,13 @@ class JokeList extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { jokes: [] };
+    this.state = { jokes: JSON.parse(window.localStorage.getItem("jokes")||"[]") };
   }
-  async componentDidMount() {
-    //Load Jokes
-    let jokes = [];
+  componentDidMount() {
+    if(this.state.jokes.length===0)this.getJokes();
+  }
+  async getJokes(){
+        let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
       let res = await axios.get("https://icanhazdadjoke.com", {
         headers: { Accept: "application/json" },
@@ -22,6 +24,10 @@ class JokeList extends Component {
       jokes.push({id:uuid(),text:res.data.joke,votes:0});
     }
     this.setState({ jokes: jokes });
+    window.localStorage.setItem(
+      "jokes",
+      JSON.stringify(jokes)
+    )
   }
   handleVote(id,delta){
     this.setState(st=>({
